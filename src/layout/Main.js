b462e5039b10954d88fe985a60eb81ba3ef2b353
salahuddin8544 <salahuddinahmed8544@gmail.com>
2022-10-25 21:59:46 +0600

    giv navbar logo

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import Outlet from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
 import { Container ,Row,Col} from 'react-bootstrap';
 import Header from '../components/Share/Header/Header';
-
 const Main = () => {
     return (
         <div>
             <Header></Header>
             <Container>
                <Row>
-                    <Col lg="4">
-                        
+                    <Col lg="4" className='bg-warning'>
+
                     </Col>
-                    <Col lg="8">
+                    <Col lg="8" className='bg-danger'>
                         <Outlet></Outlet>
                     </Col>
                </Row>
